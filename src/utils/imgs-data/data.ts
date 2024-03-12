40fbac9ed490f30e3fc034111a53ec6cb333676d
Hadaly <hadalyvf@gmail.com>
2024-03-12 21:13:52 +0100

    update: img data

---
+++ src/utils/imgs-data/data.ts
@@ -0,0 +1,502 @@
+import benitoite from '../../assets/images/benitoite.png';
+
+import azurite from '../../assets/images/azurite.jpeg';
+import potosiite2 from '../../assets/images/potosiite2.jpeg';
+import potosiite3 from '../../assets/images/potosiite3.png';
+import corkite from '../../assets/images/corkite.jpeg';
+import calcite from '../../assets/images/calcite.png';
+
+
+import chalcocite from '../../assets/images/chalcocite.jpeg';
+
+import cuprite1 from '../../assets/images/cuprite1.jpeg';
+
+import corkite2 from '../../assets/images/corkite2.jpeg';
+import biotite from '../../assets/images/biotite.png';
+
+import biotiteSm from '../../assets/images/biotite-small.png';
+
+
+import cobaltaustinite from '../../assets/images/cobaltaustinite.png';
+import cobaltaustinite2 from '../../assets/images/cobaltaustinite-small.png';
+
+import feldspar from '../../assets/images/feldspar.png';
+import fluorapatite from '../../assets/images/fluorapatite.png';
+import fluorite from '../../assets/images/fluorite.png';
+
+import beryl2 from '../../assets/images/beryl-2.jpeg';
+import beryl3 from '../../assets/images/beryl-3.jpeg';
+import beryl4 from '../../assets/images/beryl-4.jpeg';
+import beryl5 from '../../assets/images/beryl-5.jpeg';
+import beryl7 from '../../assets/images/beryl-7.jpeg';
+import fluorite2 from '../../assets/images/fluorite-2.png';
+
+
+import blueFluorite from '../../assets/images/blue-fluorite.png';
+import kunzite from '../../assets/images/kunzite.png';
+import mica4 from '../../assets/images/mica-4.jpeg';
+import opal from '../../assets/images/opal.jpeg';
+import pyrite from '../../assets/images/pyrite.jpeg';
+
+import stephanite from '../../assets/images/stephanite.png';
+import sylvite from '../../assets/images/sylvite.jpeg';
+
+import tourmaline from '../../assets/images/tourmaline.png';
+import tourmaline2 from '../../assets/images/tourmaline-2.png';
+
+import untitled1 from '../../assets/images/untitled-1.png';
+import untitled3 from '../../assets/images/untitled-3.jpeg';
+import untitled4 from '../../assets/images/untitled-4.jpeg';
+import untitled5 from '../../assets/images/untitled-5.jpeg';
+
+
+
+import gemstone from '../../assets/images/gemstone.png';
+import gypsum from '../../assets/images/gypsum.png';
+import halite from '../../assets/images/halite.png';
+import labradorite from '../../assets/images/labradorite.png';
+import mica from '../../assets/images/mica.png';
+
+import potosiite from '../../assets/images/potosiite.png';
+
+import quartz from '../../assets/images/quartz.png';
+import quartz2 from '../../assets/images/quartz-2.png';
+
+import talc from '../../assets/images/talc.png';
+
+
+import zinc from '../../assets/images/zinc.png';
+
+//rocks
+import rockAugite from '../../assets/images/rock/rock-augite.png';
+import rockFeldspar from '../../assets/images/rock/rock-feldspar.png';
+import rockGemstone from '../../assets/images/rock/rock-gemstone.png';
+import rockGypsum from '../../assets/images/rock/rock-gypsum.png';
+import rockHalite from '../../assets/images/rock/rock-halite.png';
+import rockKunzite from '../../assets/images/rock/rock-kunzite.png';
+
+import rockLabradorite from '../../assets/images/rock/rock-labradorite.png';
+import rockMalachite from '../../assets/images/rock/rock-malachite.png';
+import rockMica from '../../assets/images/rock/rock-mica.png';
+import rockNeptunite from '../../assets/images/rock/rock-neptunite.png';
+
+import rockStichite from '../../assets/images/rock/rock-stichite.png';
+import rockZinc from '../../assets/images/rock/rock-zinc.png';
+
+import rockUntd1 from '../../assets/images/rock/rock-untd1.png';
+import rockUntd2 from '../../assets/images/rock/rock-untd2.png';
+import rockUntd3 from '../../assets/images/rock/rock-untd3.png';
+import rockUntd4 from '../../assets/images/rock/rock-untd4.png';
+import rockUntd5 from '../../assets/images/rock/rock-untd5.png';
+import rockUntd6 from '../../assets/images/rock/rock-untd6.png';
+import rockUntd7 from '../../assets/images/rock/rock-untd7.png';
+import rockUntd8 from '../../assets/images/rock/rock-untd8.png';
+import rockUntd9 from '../../assets/images/rock/rock-untd9.png';
+
+//rocks for cards
+import rockAlbite from '../../assets/images/cards/rock-albite.png';
+import rockAquamarine from '../../assets/images/cards/rock-aquamarine.png';
+import rockAuralite from '../../assets/images/cards/rock-auralite.png';
+import rockAzurite from '../../assets/images/cards/rock-azurite2.png';
+
+import rockBeryl5 from '../../assets/images/cards/rock-beryl-5.png';
+import rockBeryl from '../../assets/images/cards/rock-beryl.png';
+
+import rockCalcite from '../../assets/images/cards/rock-calcite.png';
+import rockChalcocite from '../../assets/images/cards/rock-chalcocite.png';
+import rockCordierite from '../../assets/images/cards/rock-cordierite.png';
+import rockCuprite from '../../assets/images/cards/rock-cuprite.png';
+import rockCuprite2 from '../../assets/images/cards/rock-cuprite2.png';
+import rockCuprite3 from '../../assets/images/cards/rock-cuprite3.png';
+import rockCut from '../../assets/images/cards/rock-cut.png';
+import rockCutt from '../../assets/images/cards/rock-cutt.png';
+import rockEitelite from '../../assets/images/cards/rock-eitelite.png';
+import rockGalena from '../../assets/images/cards/rock-galena.png';
+import rockHematite from '../../assets/images/cards/rock-hematite.png';
+import rockJasper from '../../assets/images/cards/rock-jasper_agate.png';
+import rockOpal from '../../assets/images/cards/rock-opal.png';
+import rockOpal1 from '../../assets/images/cards/rock-opal1.png';
+import rockPegmatite from '../../assets/images/cards/rock-pegmatite.png';
+import rockRiolite from '../../assets/images/cards/rock-riolite.png';
+import rockSalt from '../../assets/images/cards/rock-salt.png';
+import rockStalactite from '../../assets/images/cards/rock-stalactite.png';
+import rockKunzite2 from '../../assets/images/cards/rock-kunzite.png';
+import rockBismuto from '../../assets/images/cards/rock-bismuto.png';
+
+
+
+
+
+
+
+
+//words
+import wordAugite from '../../assets/images/word-augite.png';
+import wordAugiteBlack from '../../assets/images/word-augite-b.png';
+
+import wordFeldspar from '../../assets/images/word-feldspar.png';
+import wordFeldsparBlack from '../../assets/images/word-feldspar-b.png';
+
+import wordGemstone from '../../assets/images/word-gemstone.png';
+import wordGemstoneBlack from '../../assets/images/word-gemstone-b.png';
+
+import wordGypsum from '../../assets/images/word-gypsum.png';
+import wordGypsumBlack from '../../assets/images/word-gypsum-b.png';
+
+import wordHalite from '../../assets/images/word-halite.png';
+import wordHaliteBlack from '../../assets/images/word-halite-b.png';
+
+import wordLabradorite from '../../assets/images/word-labradorite.png';
+import wordLabradoriteBlack from '../../assets/images/word-labradorite-b.png';
+
+import wordMalachite from '../../assets/images/word-malachite.png';
+import wordMalachiteBlack from '../../assets/images/word-malachite-b.png';
+
+import wordStichite from '../../assets/images/word-stichite.png';
+import wordStichiteBlack from '../../assets/images/word-stichite-b.png';
+
+import wordZinc from '../../assets/images/word-zinc.png';
+import wordZincBlack from '../../assets/images/word-zinc-b.png';
+
+
+export {
+  cobaltaustinite2,
+  halite,
+  biotiteSm,
+  beryl2,
+  beryl3,
+  beryl4,
+  beryl5,
+  beryl7,
+  fluorite2,
+  biotite,
+  azurite,
+  potosiite2,
+  potosiite3,
+  calcite,
+  chalcocite,
+  corkite,
+  corkite2,
+  feldspar,
+  gemstone,
+  gypsum,
+  labradorite,
+  mica,
+  quartz,
+  talc,
+  zinc,
+
+  rockAlbite,
+  rockAquamarine,
+  rockAuralite,
+  rockBeryl5,
+  rockCalcite,
+  rockChalcocite,
+  rockCordierite,
+  rockCuprite,
+  rockCuprite2,
+  rockCuprite3,
+  rockCut,
+  rockCutt,
+  rockEitelite,
+  rockGalena,
+  rockHematite,
+  rockJasper,
+  rockOpal,
+  rockOpal1,
+  rockPegmatite,
+  rockRiolite,
+  rockSalt,
+  rockStalactite,
+  rockKunzite2,
+  rockBismuto,
+
+  rockAugite,
+  rockFeldspar,
+  rockGemstone,
+  rockGypsum,
+  rockHalite,
+  rockKunzite,
+  rockLabradorite,
+  rockMalachite,
+  rockStichite,
+  rockZinc,
+  rockMica,
+  rockNeptunite,
+  rockUntd1,
+  rockUntd2,
+  rockUntd3,
+  rockUntd4,
+  rockUntd5,
+  rockUntd6,
+  rockUntd7,
+  rockUntd8,
+  rockUntd9,
+  wordAugite,
+  wordFeldspar,
+  wordGemstone,
+  wordGypsum,
+  wordHalite,
+  wordLabradorite,
+  wordMalachite,
+  wordStichite,
+  wordZinc,
+  wordAugiteBlack,
+  wordFeldsparBlack,
+  wordGemstoneBlack,
+  wordGypsumBlack,
+  wordHaliteBlack,
+  wordLabradoriteBlack,
+  wordMalachiteBlack,
+  wordStichiteBlack,
+  wordZincBlack
+};
+
+export const images = {
+  benitoite: benitoite,
+  azurite: azurite,
+  potosiite2: potosiite2,
+  potosiite3: potosiite3,
+  calcite:calcite,
+  chalcocite: chalcocite,
+  corkite: corkite,
+  corkite2: corkite2,
+  cuprite1: cuprite1,
+  beryl2: beryl2,
+  beryl3: beryl3,
+  beryl4: beryl4,
+  beryl5: beryl5,
+  beryl7: beryl7,
+  fluorite2: fluorite2,
+  blueFluorite: blueFluorite,
+  kunzite: kunzite,
+  mica4: mica4,
+  opal: opal,
+  pyrite: pyrite,
+  stephanite: stephanite,
+  sylvite: sylvite,
+  tourmaline: tourmaline,
+  tourmaline2: tourmaline2,
+  untitled1: untitled1,
+  untitled3: untitled3,
+  untitled4: untitled4,
+  untitled5: untitled5,
+
+  biotite: biotite,
+  cobaltaustinite: cobaltaustinite,
+  feldspar: feldspar,
+  fluorapatite: fluorapatite,
+  fluorite: fluorite,
+  gemstone: gemstone,
+  gypsum: gypsum,
+  halite: halite,
+  labradorite: labradorite,
+  mica: mica,
+
+  potosiite: potosiite,
+  quartz: quartz,
+  quartz2: quartz2,
+  talc: talc,
+  zinc: zinc,
+};
+
+
+export const rocks = {
+  augite: rockAugite,
+  feldspar: rockFeldspar,
+  gemstone: rockGemstone,
+  gypsum: rockGypsum,
+  halite: rockHalite,
+  labradorite: rockLabradorite,
+  malachite: rockMalachite,
+  stichite: rockStichite,
+  zinc: rockZinc,
+  kunzite: rockKunzite,
+
+  rockMica: rockMica,
+  rockNeptunite: rockNeptunite,
+  rockUntd1: rockUntd1,
+  rockUntd2: rockUntd2,
+  rockUntd3: rockUntd3,
+  rockUntd4: rockUntd4,
+  rockUntd5: rockUntd5,
+  rockUntd6: rockUntd6,
+  rockUntd7: rockUntd7,
+  rockUntd8: rockUntd8,
+  rockUntd9: rockUntd9,
+
+  rockAlbite: rockAlbite,
+  rockAquamarine: rockAquamarine,
+  rockAuralite: rockAuralite,
+  rockAzurite: rockAzurite,
+  rockBeryl5: rockBeryl5,
+  rockBeryl: rockBeryl,
+
+  rockCalcite: rockCalcite,
+  rockChalcocite: rockChalcocite,
+  rockCordierite: rockCordierite,
+  rockCuprite: rockCuprite,
+  rockCuprite2: rockCuprite2,
+  rockCuprite3: rockCuprite3,
+  rockCut: rockCut,
+  rockCutt: rockCutt,
+  rockEitelite: rockEitelite,
+  rockGalena: rockGalena,
+  rockHematite: rockHematite,
+  rockJasper: rockJasper,
+  rockOpal: rockOpal,
+  rockOpal1: rockOpal1,
+  rockPegmatite: rockPegmatite,
+  rockRiolite: rockRiolite,
+  rockSalt: rockSalt,
+  rockStalactite: rockStalactite,
+  rockKunzite2: rockKunzite2,
+  rockBismuto: rockBismuto
+};
+
+export const words = {
+  augite: wordAugite,
+  feldspar: wordFeldspar,
+  gemstone: wordGemstone,
+  gypsum: wordGypsum,
+  halite: wordHalite,
+  labradorite: wordLabradorite,
+  malachite: wordMalachite,
+  stichite: wordStichite,
+  zinc: wordZinc,
+};
+
+export const wordsBlack = {
+  augite: wordAugiteBlack,
+  feldspar: wordFeldsparBlack,
+  gemstone: wordGemstoneBlack,
+  gypsum: wordGypsumBlack,
+  halite: wordHaliteBlack,
+  labradorite: wordLabradoriteBlack,
+  malachite: wordMalachiteBlack,
+  stichite: wordStichiteBlack,
+  zinc: wordZincBlack,
+};
+
+export const imgInteractionData = [
+  {
+    id: 1,
+    title: "Fluorite",
+    img: images.fluorite
+
+  },
+  {
+    id: 2,
+    title: "Anorithe",
+    img: images.labradorite
+
+  },
+  {
+    id: 3,
+    title: "Potosiite",
+    img: images.potosiite2
+   
+  },
+
+  {
+    id: 4,
+    title: "Feldspar",
+    img: images.beryl7
+   
+
+  },
+  {
+    id: 5,
+    title: "Talcum",
+    img: images.talc
+  },
+  {
+    id: 6,
+    title: "Halite",
+    img: images.untitled3
+
+  },
+  {
+    id: 7,
+    title: "Corkite",
+    img: images.corkite2
+  },
+  {
+    id: 8,
+    title: "Fluoratite",
+    img: images.fluorapatite
+  },
+  {
+    id: 9,
+    title: "Halite",
+    img: images.untitled1
+  },
+  {
+    id: 10,
+    title: "Zinc",
+    img: images.zinc
+  },
+  {
+    id: 11,
+    title: "Sylvite",
+    img: images.sylvite
+  },
+  {
+    id: 12,
+    title: "Tourmaline",
+    img: images.tourmaline
+  },
+  {
+    id: 13,
+    title: "Celestite",
+    img: images.blueFluorite
+  },
+  {
+    id: 14,
+    title: "Chalcocite",
+    img: images.chalcocite
+ 
+  },
+  {
+    id: 15,
+    title: "Cuprite",
+    img: images.cuprite1
+  },
+
+  {
+    id: 16,
+    title: "Beryl",
+    img: images.beryl5
+  },
+  {
+    id: 17,
+    title: "Pyrite",
+    img: images.pyrite
+  },
+  {
+    id: 18,
+    title: "Mica",
+    img: images.mica
+  },
+  {
+    id: 19,
+    title: "Gypsum",
+    img: images.gypsum
+  },
+  {
+    id: 20,
+    title: "Calcite",
+    img: images.calcite
+  },
+  // {
+  //   id: 16,
+  //   title: "Biotite",
+  //   img: images.biotite
+  // },
+  {
+    id: 17,
+    title: "Azurite",
+    img: images.azurite
+  },
+];
+
---
+++ src/utils/imgs-data/rocks.ts
@@ -0,0 +1,72 @@
+import { rocks } from "./data";
+
+export const rocksData = [
+  {
+    name: "Kunzite",
+    width: 290,
+    height: 175,
+    img: rocks.kunzite
+  },
+  {
+    name: "Mica",
+    width: 307,
+    height: 338,
+    img: rocks.rockMica
+  },
+  {
+    name: "Untd1",
+    width: 293,
+    height: 283,
+    img: rocks.rockUntd1
+  },
+  {
+    name: "Untd2",
+    width: 300,
+    height: 197,
+    img: rocks.rockUntd2
+  },
+  {
+    name: "Untd3",
+    width: 151,
+    height: 310,
+    img: rocks.rockUntd3
+  },
+  {
+    name: "Untd4",
+    width: 302,
+    height: 283,
+    img: rocks.rockUntd4
+  },
+  // {
+  //   name: "Untd5",
+  //   width: 269,
+  //   height: 255,
+  //   img: rocks.rockUntd5
+  // },
+  {
+    name: "Untd6",
+    width: 309,
+    height: 274,
+    img: rocks.rockUntd6
+  },
+  {
+    name: "Untd7",
+    width: 305,
+    height: 239,
+    img: rocks.rockUntd7
+  },
+  // {
+  //   name: "Untd8",
+  //   width: 295,
+  //   height: 160,
+  //   img: rocks.rockUntd8
+  // },
+  {
+    name: "Untd9",
+    width: 308,
+    height: 176,
+    img: rocks.rockUntd9
+  },
+
+
+];
